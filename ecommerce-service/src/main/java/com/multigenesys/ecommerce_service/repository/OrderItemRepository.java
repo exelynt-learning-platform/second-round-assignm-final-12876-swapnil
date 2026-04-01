2352ba166b226ef5ef658c9d912165129b7c595b
Swapnil-Jagtap <152247426+swapniljagtap2303@users.noreply.github.com>
2026-04-01 20:43:03 +0530

    Delete ecommerce-service directory

--- ecommerce-service/src/main/java/com/multigenesys/ecommerce_service/repository/OrderItemRepository.java
+++
@@ -1,7 +0,0 @@
-package com.multigenesys.ecommerce_service.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-import com.multigenesys.ecommerce_service.entity.OrderItem;
-
-public interface OrderItemRepository extends JpaRepository<OrderItem, Long> {
-}
