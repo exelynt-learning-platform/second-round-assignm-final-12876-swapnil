b63727127923d6dd3f9b7be8aa23970b5e34dbbb
Swapnil-Jagtap <jagtapswapnil2303@gmail.com>
2026-04-01 19:56:26 +0530

    Clean Repository

--- AuthService/src/main/java/com/multigenesys/AuthService/entity/Role.java
+++
@@ -1,7 +0,0 @@
-package com.multigenesys.AuthService.entity;
-
-public enum Role {
-	
-    USER,
-    ADMIN
-}
--- ecommerce-service/src/main/java/com/multigenesys/ecommerce_service/repository/OrderRepository.java
+++
@@ -1,7 +0,0 @@
-package com.multigenesys.ecommerce_service.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-import com.multigenesys.ecommerce_service.entity.Order;
-
-public interface OrderRepository extends JpaRepository<Order, Long> {
-}
