5005414f7b9056142a1ef5e1035e1208b9b80900
Swapnil-Jagtap <152247426+swapniljagtap2303@users.noreply.github.com>
2026-04-01 20:42:45 +0530

    Delete AuthService directory

--- AuthService/src/main/java/com/multigenesys/AuthService/repository/UserLoginHistoryRepository.java
+++
@@ -1,8 +0,0 @@
-package com.multigenesys.AuthService.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-import com.multigenesys.AuthService.entity.UserLoginHistory;
-
-public interface UserLoginHistoryRepository 
-       extends JpaRepository<UserLoginHistory, Long> {
-}
