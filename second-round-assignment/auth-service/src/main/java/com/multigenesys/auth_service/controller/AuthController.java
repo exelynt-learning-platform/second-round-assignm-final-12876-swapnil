cb28434424f4662633137a319271d3e661cb2dc1
Swapnil-Jagtap <jagtapswapnil2303@gmail.com>
2026-04-02 18:49:57 +0530

    duplicate exception handling

--- second-round-assignment/auth-service/src/main/java/com/multigenesys/auth_service/controller/AuthController.java
+++ second-round-assignment/auth-service/src/main/java/com/multigenesys/auth_service/controller/AuthController.java
@@ -26,47 +26,38 @@ public class AuthController {
 	@PostMapping("/register")
 	public ResponseEntity<ApiResponse<Void>> register(@RequestBody RegisterRequest request) {
 
-		try {
-			authService.register(request);
+	    try {
+	        authService.register(request);
 
-			return ResponseEntity.status(HttpStatus.CREATED)
-					.body(new ApiResponse<>("User registered successfully", "SUCCESS", null));
+	        return ResponseEntity.status(HttpStatus.CREATED)
+	                .body(new ApiResponse<>("User registered successfully", "SUCCESS", null));
 
-		} catch (RuntimeException ex) {
+	    } catch (Exception ex) {
 
-			return ResponseEntity.status(HttpStatus.BAD_REQUEST)
-					.body(new ApiResponse<>(ex.getMessage(), "ERROR", null));
-
-		} catch (Exception ex) {
-
-			return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR)
-					.body(new ApiResponse<>("Something went wrong", "ERROR", null));
-		}
+	        return ResponseEntity.status(HttpStatus.BAD_REQUEST)
+	                .body(new ApiResponse<>(ex.getMessage(), "ERROR", null));
+	    }
 	}
 
 	@PostMapping("/login")
 	public ResponseEntity<ApiResponse<AuthResponse>> login(@RequestBody LoginRequest request,
-			HttpServletRequest httpRequest) {
+	        HttpServletRequest httpRequest) {
 
-		try {
+	    try {
 
-			String ipAddress = httpRequest.getRemoteAddr();
-			String userAgent = httpRequest.getHeader("User-Agent");
+	        String ipAddress = httpRequest.getRemoteAddr();
+	        String userAgent = httpRequest.getHeader("User-Agent");
 
-			AuthResponse token = authService.login(request, ipAddress, userAgent);
+	        AuthResponse token = authService.login(request, ipAddress, userAgent);
 
-			return ResponseEntity.ok(new ApiResponse<>("Login successful", "SUCCESS", token));
+	        return ResponseEntity.ok(new ApiResponse<>("Login successful", "SUCCESS", token));
 
-		} catch (RuntimeException ex) {
+	    } catch (Exception ex) {
 
-			return ResponseEntity.status(HttpStatus.UNAUTHORIZED)
-					.body(new ApiResponse<>(ex.getMessage(), "ERROR", null));
-
-		} catch (Exception ex) {
-
-			return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR)
-					.body(new ApiResponse<>("Something went wrong", "ERROR", null));
-		}
+	        return ResponseEntity.status(HttpStatus.UNAUTHORIZED)
+	                .body(new ApiResponse<>(ex.getMessage(), "ERROR", null));
+	    }
 	}
+	
 
 }
